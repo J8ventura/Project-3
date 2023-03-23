e20dbce946ac792b33fb5b5ab5e74536cc373398
J8ventura <jventur5@asu.edu>
2023-03-23 08:56:34 -0500

    submission

--- static/js/main.js
+++ static/js/main.js
@@ -1,84 +1,23 @@
 console.log("hello world")
-// get all add to cart buttons
-const addToCartBtns = document.querySelectorAll('.add_cart_btn');
 
-// loop through each button and add a click event listener
-addToCartBtns.forEach((btn) => {
-  btn.addEventListener('click', () => {
-    // get product details from clicked button's parent div
-    const product = btn.parentNode.parentNode;
-    const id = product.dataset.id;
-    const name = product.querySelector('.title').textContent;
-    const price = parseFloat(product.querySelector('.amount').textContent.replace('$', ''));
-    const image = product.querySelector('img').src;
 
-    // create new cart item object
-    const item = {
-      id,
-      name,
-      price,
-      image,
-      quantity: 1
-    };
+const form = document.getElementById("search-form");
+const input = document.getElementById("search-input");
 
-    // get existing cart data from local storage
-    const cart = JSON.parse(localStorage.getItem('cart')) || { items: [] };
+form.addEventListener("submit", function(event) {
+  event.preventDefault(); // prevent the form from submitting
+  
+  const searchQuery = input.value;
 
-    // check if item already exists in cart
-    const existingItem = cart.items.find((i) => i.id === id);
+  let pageUrl = "";
 
-    if (existingItem) {
-      // if item already exists, update quantity
-      existingItem.quantity++;
-    } else {
-      // if item doesn't exist, add new item to items array
-      cart.items.push(item);
-    }
+  if (searchQuery === "rugs") {
+    pageUrl = "http://127.0.0.1:8000/rugs/";
+  } else if (searchQuery === "throw pillows") {
+    pageUrl = "http://127.0.0.1:8000/throw_pillows/";
+  } else {
+    pageUrl = "http://127.0.0.1:8000/";
+  }
 
-    // update cart in local storage
-    localStorage.setItem('cart', JSON.stringify(cart));
-
-    // display success message
-    alert(`${name} added to cart`);
-
-  });
+  window.location.href = pageUrl; // redirect to the appropriate page
 });
-
-// display cart items on shopping cart page
-const cartItemsContainer = document.querySelector('.cart-items');
-
-// get existing cart data from local storage
-const cart = JSON.parse(localStorage.getItem('cart')) || { items: [] };
-
-// create cart items HTML
-let cartItemsHtml = '';
-
-// loop through each item and add HTML
-cart.items.forEach((item) => {
-  cartItemsHtml += `
-    <div class="cart-item">
-      <img src="${item.image}" alt="${item.name}">
-      <div class="cart-item-details">
-        <h3>${item.name}</h3>
-        <p>Price: $${item.price.toFixed(2)}</p>
-        <p>Quantity: ${item.quantity}</p>
-      </div>
-    </div>
-  `;
-});
-
-// add cart items HTML to container
-cartItemsContainer.innerHTML = cartItemsHtml;
-
-// display cart total on shopping cart page
-const cartTotalContainer = document.querySelector('.cart-total');
-
-// calculate cart total
-let cartTotal = 0;
-
-cart.items.forEach((item) => {
-  cartTotal += item.price * item.quantity;
-});
-
-// add cart total HTML to container
-cartTotalContainer.innerHTML = `$${cartTotal.toFixed(2)}`;
